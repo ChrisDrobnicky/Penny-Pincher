10be25e876327d1e80dd565e7d9164488c510b5c
ChrisDrobnicky <krizzztofer@yahoo.com>
2017-05-13 20:36:34 +0200

    introduce jquery in accounts

--- public/views/accounts/accounts.js
+++ public/views/accounts/accounts.js
@@ -1,59 +1,57 @@
 function addAccountToDom (name, balance){
-  var accountList = document.querySelector('#accountList');
+  var $accountList = $('#accountList');
   var accountID = manageLocalStorage.getAccountID();
-  var newListItem = document.createElement('li');
-  var newAccountName = document.createElement('span');
-  var newAccountBalance = document.createElement('span');
-  var deleteButton = document.createElement('button');
-  var deleteIcon = document.createElement('span');
-  newListItem.classList.add('pp-account-list__item');
-  newListItem.setAttribute('id', accountID);
-  newAccountName.textContent = name;
-  newAccountName.classList.add('pp-account-list__name');
-  newAccountBalance.textContent = balance;
-  newAccountBalance.classList.add('pp-account-list__balance');
-  deleteButton.classList.add('btn', 'btn-danger', 'btn-remove', 'pp-account-list__button');
-  deleteIcon.classList.add('glyphicon', 'glyphicon-minus');
-  deleteButton.addEventListener('click', removeAccount, false);
+  var $newListItem = $(document.createElement('li'));
+  var $newAccountName = $(document.createElement('span'));
+  var $newAccountBalance = $(document.createElement('span'));
+  var $deleteButton = $(document.createElement('button'));
+  var $deleteIcon = $(document.createElement('span'));
+  $newListItem.addClass('pp-account-list__item').attr('id', accountID);
+  $newAccountName.text(name).addClass('pp-account-list__name');
+  $newAccountBalance.text(balance).addClass('pp-account-list__balance');
+  $deleteButton.addClass('btn btn-danger btn-remove pp-account-list__button');
+  $deleteIcon.addClass('glyphicon glyphicon-minus');
 
-  deleteButton.appendChild(deleteIcon);
-  newListItem.appendChild(newAccountName);
-  newListItem.appendChild(newAccountBalance);
-  newListItem.appendChild(deleteButton);
-  accountList.appendChild(newListItem);
+  $deleteButton.click(removeAccount);
+
+  $deleteButton.append($deleteIcon);
+  $newListItem.append($newAccountName);
+  $newListItem.append($newAccountBalance);
+  $newListItem.append($deleteButton);
+  $accountList.append($newListItem);
 }
 
-function removeAccountFromDom (listItem){
-  listItem.parentNode.removeChild(listItem);
+function removeAccountFromDom(listItem){
+  $(listItem).remove();
 }
 
 function checkInput(accountName, accountBalance){
-  var outputDiv = document.querySelector('#outputDiv');
+  var $outputDiv = $('#outputDiv');
   if (accountName.length === 0 || accountBalance.length === 0) {
-    outputDiv.classList.add('alert', 'alert-danger');
-    outputDiv.innerHTML = '<strong>Error in your form:</strong> Please enter the gaps';
+    $outputDiv.addClass('alert alert-danger');
+    $outputDiv.html('<strong>Error in your form:</strong> Please enter the gaps');
     return false;
   } else if (isNaN(accountBalance)){
-    outputDiv.classList.add('alert', 'alert-danger');
-    outputDiv.innerHTML = '<strong>Error in your form:</strong> Your balance must be a number - e.g. 0.00';
+    $outputDiv.addClass('alert alert-danger');
+    $outputDiv.html ('<strong>Error in your form:</strong> Your balance must be a number - e.g. 0.00');
     return false;
   } else {
-    outputDiv.classList.remove('alert', 'alert-danger');
-    outputDiv.textContent = '';
+    $outputDiv.removeClass('alert alert-danger');
+    $outputDiv.text('');
     return true;
   }
 }
 
 function clearFormInputs() {
-  var nameInput = document.querySelector('#account-name');
-  var balanceInput = document.querySelector('#account-balance');
-  nameInput.value = '';
-  balanceInput.value = '';
+  var $nameInput = $('#account-name');
+  var $balanceInput = $('#account-balance');
+  $nameInput.val('');
+  $balanceInput.val('');
 }
 
 function saveAccount() {
-  var accountName = document.querySelector('#account-name').value;
-  var accountBalance = document.querySelector('#account-balance').value;
+  var accountName = $('#account-name').val();
+  var accountBalance = $('#account-balance').val();
   var accountID = localStorage.getItem('accountID');
   checkInput(accountName, accountBalance);
   if (checkInput(accountName, accountBalance) === true) {
@@ -77,8 +75,8 @@ function getAccountsFromStorage () {
 }
 
 function onPageLoaded() {
-  var addButton = document.querySelector('#add-button');
-  addButton.addEventListener('click', saveAccount, false);
+  var $addButton = $('#add-button');
+  $addButton.click(saveAccount);
   getAccountsFromStorage();
   if (localStorage.getItem('accountID')=== null ) {
     manageLocalStorage.generateAccountID();
